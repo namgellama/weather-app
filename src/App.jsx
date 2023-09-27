f9c062b38b5c2dfec1c938d9068d58f6387fd555
Namgel <namgeltamang14@gmail.com>
2023-09-27 17:20:15 +0545

    call weather api

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import axios from 'axios';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
+import { AiOutlineSearch } from 'react-icons/ai';
 
 function App() {
 	const API_KEY = 'a4042cb5040cb68342db86a1995d773a';
 
 	const [input, setInput] = useState('');
-	const [weatherData, setWeatherData] = useState([]);
+	const [weatherData, setWeatherData] = useState(null);
 	const [city, setCity] = useState('');
 
 	const getCityData = async () => {
@@ -30,34 +31,59 @@ function App() {
 		}
 	};
 
-	const submitHandler = async () => {
+	const submitHandler = async (e) => {
+		e.preventDefault();
 		const res = await getCityData();
 		const { lat, lon, name } = res[0];
 
-		const { weather } = await getWeatherData(lat, lon);
-		setWeatherData(weather);
+		const data = await getWeatherData(lat, lon);
+		setWeatherData(data);
 		setCity(name);
 	};
 
 	return (
-		<>
-			<input type="text" onChange={(e) => setInput(e.target.value)} />
-			<button onClick={submitHandler}>Submit</button>
+		<div className="my-20 w-[1000px] mx-auto flex justify-center flex-col items-center">
+			<form
+				className="border pl-5 pr-3 py-2 rounded-2xl bg-white flex items-center justify-between gap-x-3 w-[40%] mb-10"
+				onSubmit={submitHandler}
+			>
+				<input
+					type="text"
+					className="outline-none"
+					onChange={(e) => setInput(e.target.value)}
+				/>
+				<button type="submit">
+					<AiOutlineSearch className="text-lg" />
+				</button>
+			</form>
 
-			{weatherData.length ? (
-				<div>
-					<h1>Weather in {city}</h1>
+			<div>
+				{/* {!weatherData && <h2>Loading...</h2>} */}
 
-					{weatherData &&
-						weatherData.map((el) => (
+				{city && <h1 className="text-3xl font-bold">Weather in {city}</h1>}
+				{weatherData && (
+					<div className="my-5 text-lg">
+						{weatherData?.weather.map((el) => (
 							<div key={el.id}>
-								<h1>{el.main}</h1>
-								<p>{el.description}</p>
+								<h1>Title: {el.main}</h1>
+								<p>Description: {el.description}</p>
 							</div>
 						))}
-				</div>
-			) : null}
-		</>
+						<h1>
+							Wind Speed: {weatherData.wind.speed}, Wind Degree:{' '}
+							{weatherData.wind.speed}
+						</h1>
+						<p>Temperature: {weatherData.main.temp}</p>
+						<p className="text-lg ">
+							Min Temperature: {weatherData.main.temp_min}
+						</p>
+						<p>Maximum Temperature: {weatherData.main.temp_max}</p>
+						<p>Pressure: {weatherData.main.pressure}</p>
+						<p>Humidity: {weatherData.main.humidity}</p>
+					</div>
+				)}
+			</div>
+		</div>
 	);
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import { Provider } from 'react-redux';
-import { legacy_createStore, combineReducers, applyMiddleware } from 'redux';
-import createSagaMiddleware from '@redux-saga/core';
-
-// const sagaMiddleware = createSagaMiddleware();
-// const rootReducer = combineReducers({});
-
-// const store = legacy_createStore(rootReducer, applyMiddleware(sagaMiddleware));
-// sagaMiddleware.run()
+// import store from './redux/configureStore.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
